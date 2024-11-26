0fdf50783ee3a304169b6f792fbbb7112f8b9ed7
Demetrius Lewis <Demetriuslewis2012@gmail.com>
2024-11-26 10:59:17 -0500

    feat: setup laser class and laser timer working

--- main.py
+++ main.py
@@ -5,6 +5,35 @@ class Ship(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load('./assets/graphics/ship.png').convert_alpha()
         self.rect = self.image.get_rect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+        self.can_shoot = True
+        self.shoot_time = None
+
+    def input_position(self):
+        self.rect.center = pygame.mouse.get_pos()
+
+    def laser_shoot(self):
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            print('shoot laser')
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+
+    def laser_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time > 500:
+                self.can_shoot = True
+
+
+    def update(self):
+        self.input_position()
+        self.laser_timer()
+        self.laser_shoot()
+
+class Laser(pygame.sprite.Sprite):
+    def __init__(self, groups, pos):
+        super().__init__(groups)
+        self.image = pygame.image.load('./assets/graphics/laser.png').convert_alpha()
+        self.rect = self.image.get_rect(midbottom = pos)
 
 
 
@@ -20,8 +49,11 @@ clock = pygame.time.Clock()
 
 background_surf = pygame.image.load('./assets/graphics/background.png').convert_alpha()
 
-spaceship_group = pygame.sprite.Group()
+spaceship_group = pygame.sprite.GroupSingle()
+laser_group = pygame.sprite.Group()
+
 ship = Ship(spaceship_group)
+laser = Laser(laser_group, (100, 300))
 
 #game loop
 while True:
@@ -35,9 +67,14 @@ while True:
     #delta
     dt = clock.tick() / 1000
 
+    #update
+    spaceship_group.update()
+
     #graphics
     display_surface.blit(background_surf, (0, 0))
     spaceship_group.draw(display_surface)
+    laser_group.draw(display_surface)
+
 
     #draw frame
     pygame.display.update()
